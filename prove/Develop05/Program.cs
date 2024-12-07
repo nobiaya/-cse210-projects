665a366e21f0df7850dcd09df6be0d0a3a5571e6
Lily <lilypromise99@gmail.com>
2024-12-07 17:26:23 -0200

    added project to develop 5

--- prove/Develop05/Program.cs
+++ prove/Develop05/Program.cs
@@ -1,9 +1,187 @@
 using System;
+using System.Collections.Generic;
+using System.Threading;
+
+abstract class Activity
+{
+    protected string Name;
+    protected string Description;
+    private int Duration;
+
+    public void Start()
+    {
+        Console.Clear();
+        Console.WriteLine($"Welcome to the {Name}!\n");
+        Console.WriteLine(Description);
+        Console.Write("Enter the duration of the activity in seconds: ");
+        Duration = int.Parse(Console.ReadLine());
+        Console.WriteLine("\nPrepare to begin...");
+        DisplaySpinner(3);
+        PerformActivity(Duration);
+        Console.WriteLine("\nGood job!");
+        Console.WriteLine($"You have completed the {Name} for {Duration} seconds.");
+        DisplaySpinner(3);
+    }
+
+    protected abstract void PerformActivity(int duration);
+
+    protected void DisplaySpinner(int seconds)
+    {
+        for (int i = 0; i < seconds * 4; i++)
+        {
+            Console.Write("|/-\\"[i % 4]);
+            Thread.Sleep(250);
+            Console.Write("\b");
+        }
+    }
+
+    protected void Countdown(int seconds)
+    {
+        for (int i = seconds; i > 0; i--)
+        {
+            Console.Write($"{i} ");
+            Thread.Sleep(1000);
+            Console.Write("\b\b\b");
+        }
+    }
+}
+
+class BreathingActivity : Activity
+{
+    public BreathingActivity()
+    {
+        Name = "Breathing Activity";
+        Description = "This activity will help you relax by walking you through breathing in and out slowly. Clear your mind and focus on your breathing.";
+    }
+
+    protected override void PerformActivity(int duration)
+    {
+        int cycles = duration / 6;
+        for (int i = 0; i < cycles; i++)
+        {
+            Console.WriteLine("Breathe in...");
+            Countdown(3);
+            Console.WriteLine("Breathe out...");
+            Countdown(3);
+        }
+    }
+}
+
+class ReflectionActivity : Activity
+{
+    private static readonly List<string> Prompts = new List<string>
+    {
+        "Think of a time when you stood up for someone else.",
+        "Think of a time when you did something really difficult.",
+        "Think of a time when you helped someone in need.",
+        "Think of a time when you did something truly selfless."
+    };
+
+    private static readonly List<string> Questions = new List<string>
+    {
+        "Why was this experience meaningful to you?",
+        "Have you ever done anything like this before?",
+        "How did you get started?",
+        "How did you feel when it was complete?",
+        "What made this time different than other times when you were not as successful?",
+        "What is your favorite thing about this experience?",
+        "What could you learn from this experience that applies to other situations?",
+        "What did you learn about yourself through this experience?",
+        "How can you keep this experience in mind in the future?"
+    };
+
+    public ReflectionActivity()
+    {
+        Name = "Reflection Activity";
+        Description = "This activity will help you reflect on times in your life when you have shown strength and resilience. This will help you recognize the power you have and how you can use it in other aspects of your life.";
+    }
+
+    protected override void PerformActivity(int duration)
+    {
+        Random random = new Random();
+        Console.WriteLine(Prompts[random.Next(Prompts.Count)]);
+        Console.WriteLine("\nReflect on the following questions:");
+
+        int timePerQuestion = duration / Questions.Count;
+        foreach (var question in Questions)
+        {
+            if (duration <= 0) break;
+            Console.WriteLine(question);
+            DisplaySpinner(timePerQuestion);
+            duration -= timePerQuestion;
+        }
+    }
+}
+
+class ListingActivity : Activity
+{
+    private static readonly List<string> Prompts = new List<string>
+    {
+        "Who are people that you appreciate?",
+        "What are personal strengths of yours?",
+        "Who are people that you have helped this week?",
+        "When have you felt the Holy Ghost this month?",
+        "Who are some of your personal heroes?"
+    };
+
+    public ListingActivity()
+    {
+        Name = "Listing Activity";
+        Description = "This activity will help you reflect on the good things in your life by having you list as many things as you can in a certain area.";
+    }
+
+    protected override void PerformActivity(int duration)
+    {
+        Random random = new Random();
+        Console.WriteLine(Prompts[random.Next(Prompts.Count)]);
+        Console.WriteLine("\nYou have a few seconds to prepare...");
+        Countdown(3);
+
+        List<string> items = new List<string>();
+        DateTime endTime = DateTime.Now.AddSeconds(duration);
+
+        while (DateTime.Now < endTime)
+        {
+            Console.Write("Enter an item: ");
+            items.Add(Console.ReadLine());
+        }
+
+        Console.WriteLine($"\nYou listed {items.Count} items.");
+    }
+}
 
 class Program
 {
-    static void Main(string[] args)
+    static void Main()
     {
-        Console.WriteLine("Hello Develop05 World!");
+        while (true)
+        {
+            Console.Clear();
+            Console.WriteLine("Select an activity:");
+            Console.WriteLine("1. Breathing Activity");
+            Console.WriteLine("2. Reflection Activity");
+            Console.WriteLine("3. Listing Activity");
+            Console.WriteLine("4. Quit");
+            Console.Write("Enter your choice: ");
+
+            switch (Console.ReadLine())
+            {
+                case "1":
+                    new BreathingActivity().Start();
+                    break;
+                case "2":
+                    new ReflectionActivity().Start();
+                    break;
+                case "3":
+                    new ListingActivity().Start();
+                    break;
+                case "4":
+                    return;
+                default:
+                    Console.WriteLine("Invalid choice. Try again.");
+                    Thread.Sleep(1000);
+                    break;
+            }
+        }
     }
 }
